e62196bc0ce30462707e56d475bf8f64c44ce41e
Bill Hunter <Bill.Hunter@mensic.com>
2025-08-05 15:25:04 -0400

    Netlify basic auth working locally

---
+++ netlify/edge-functions/basic-auth.ts
@@ -0,0 +1,3 @@
+// netlify/edge-functions/basic-auth.ts
+
+export { handler as default } from 'https://deno.land/x/netlify_basic_auth_edge_function@v1.0.0/mod.ts'
---
+++ netlify/edge-functions/hello.js
@@ -0,0 +1,3 @@
+export default () => new Response("Hello from the edge");
+
+export const config = { path: "/test" };
